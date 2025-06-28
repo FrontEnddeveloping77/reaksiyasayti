b0758077c825027437afc71e5004eb756ce704e4
FrontEnddeveloping77 <frontenddeveloping1@gmail.com>
2025-06-28 14:57:44 +0500

    reaksiya sayti

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("scroll-btn").addEventListener("click", function () {
+    document.querySelector(".second").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("scroll-btn2").addEventListener("click", function () {
+    document.querySelector(".third").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("scroll-btn3").addEventListener("click", function () {
+    document.querySelector(".fourth").scrollIntoView({
+        behavior: "smooth"
+    });
+});
